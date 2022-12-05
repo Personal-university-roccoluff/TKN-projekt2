6fc589d26850cf7eb9b5c416715fb30fc139f6f2
Rocco Luffarelli <rocco@luffarelli.it>
2022-12-05 13:49:40 +0100

    Given files to start 1.1

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,6 +3,7 @@
 void htable_set(htable **ht, const unsigned char *key, size_t key_len,
                 const unsigned char *value, size_t value_len) {
     /* TODO IMPLEMENT */
+    // TEST TEST TEST
 }
 
 htable *htable_get(htable **ht, const unsigned char *key, size_t key_len) {
